752fd065e6b80d2dcd13d631a708693cb2838a5a
Sclipper <mkopoi3@gmail.com>
2019-02-18 16:32:21 +0100

    Day 1

--- app/src/main/java/dk/kea/class2019/tsbat/gameengine19/GameEngine.java
+++ app/src/main/java/dk/kea/class2019/tsbat/gameengine19/GameEngine.java
@@ -3,18 +3,19 @@ package dk.kea.class2019.tsbat.gameengine19;
 import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class GameEngine extends AppCompatActivity {
+public abstract class GameEngine extends AppCompatActivity implements Runnable {
 
     private Thread mainLoopThread;
     private State state = State.Paused;
     private List<State> stateChanges = new ArrayList<>();
-    
+
 
     public abstract Screen createStartScreen();
 
@@ -65,6 +66,40 @@ public abstract class GameEngine extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     }
 
+    public void run()
+    {
+        while(true)
+        {
+            synchronized(stateChanges)
+            {
+                for(int i = 0; i < stateChanges.size(); i++ )
+                {
+                    state = stateChanges.get(i);
+                    if(state == State.Disposed)
+                    {
+                        Log.d("GameEngine", "state changed to Disposed");
+                        return;
+                    }
+                    if(state == State.Paused)
+                    {
+                        Log.d("GameEngine", "state changed to Paused");
+                        return;
+                    }
+                    if(state == State.Resumed)
+                    {
+                        Log.d("GameEngine", "state changed to Resumed");
+                    }
+//                    if(state == State.Running)
+//                    {
+//                        Log.d("GameEngine", "state changed to Running");
+//                        return;
+//                    }
+                }
+                stateChanges.clear();
+            }
+        }
+    }
+
     public void onPause()
     {
         super.onPause();
@@ -73,5 +108,11 @@ public abstract class GameEngine extends AppCompatActivity {
     public void onResume()
     {
         super.onResume();
+        mainLoopThread = new Thread(this);
+        mainLoopThread.start();
+        synchronized (stateChanges)
+        {
+
+        }
     }
 }
